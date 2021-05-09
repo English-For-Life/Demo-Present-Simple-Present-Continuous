9033182c48e43edcb2564113f52fda0d58a29a94
English-For-Life <83182532+English-For-Life@users.noreply.github.com>
2021-05-09 21:37:37 +0300

    Add files via upload

---
+++ questions.js
@@ -0,0 +1,28 @@
+//Array of objects
+const quiz = [
+    {
+        q:"She ___ English at the moment.",
+        options:["doesn't learn", "don't learn", "isn't learning"],
+        answer: 2
+    },
+    {
+        q:"I ___ the dog for a walk every day.",
+        options:["don't take", "doesn't take", "'m not taking"],
+        answer: 0
+    },
+    {
+        q:"What time ___ they ___ ?",
+        options:["does / gets up", "do / get up", "are / getting up"],
+        answer: 1
+    },
+    {
+        q:"Where ___ they ___ ? In a house or a flat?",
+        options:["are / living", "does / lives", "do / live"],
+        answer: 2
+    },
+    {
+        q:"Sam ___ now.",
+        options:["isn't running", "doesn't run", "aren't running"],
+        answer: 0
+    }
+]
